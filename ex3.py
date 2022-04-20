7aa0c6b580176c016009187780b5d0203eb17bb9
Fernanda Fernandes <89754730+FernandaFernandes94@users.noreply.github.com>
2022-04-19 21:32:05 -0300

    Add files via upload
    Primeiro exercício

---
+++ ex3.py
@@ -0,0 +1,26 @@
+precopao = 0.50
+precobroa = 1.50
+
+
+pao = float (input ("informe a quantia de paẽs vendidos"))
+
+broa = float (input ("informe a quantia de broas vendidas"))
+#total em paes
+tp = pao * precopao
+#total de broas
+tb = broa * precobroa
+
+#totais de vendas
+totalvenda = tp + tb
+
+print("o total das vendas é: " , round(totalvenda , 2) )
+
+poupanca = totalvenda 
+
+
+
+print (" Valor guardado na poupança é: " , round(poupanca , 2) )
+
+
+
+
